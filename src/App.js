78167fbf36ca021192600be93a60df447c916f8a
Robert S <robscafe333@yahoo.com>
2022-09-19 10:07:39 -0700

    Practicing routes and route, react router.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,7 @@
-import "./categories.styles.scss";
+import Home from "./routes/home/home.component";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneaker",
-    },
-    {
-      id: 4,
-      title: "Womens",
-    },
-    {
-      id: 5,
-      title: "Mens",
-    },
-  ];
-  return (
-    <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
-          <div className="background-image"></div>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <Home />;
 };
 
 export default App;
